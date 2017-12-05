32c3ce99d73d1b46e7958773e3cdf638b0e82475
Mateus Resende <mateus.rsnd@gmail.com>
2017-12-05 10:10:17 -0200

    Initializing electronJS

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="app-footer">
+                <span>Feito por Mateus Resende</span>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
