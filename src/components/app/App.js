2fb39bb7e970f631fc4fec8f7dcd256db16ac681
Mateus Resende <mateus.rsnd@gmail.com>
2017-12-07 11:24:41 -0200

    Decided to use files written in JSON for the database

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Body from '../body/Body';
-
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+    tealA700, teal500, teal700, grey400, grey100, grey500
+} from 'material-ui/styles/colors';
 import './app.css';
 
 class App extends Component {
+    muiTheme = getMuiTheme({
+        palette: {
+            height: 60,
+            primary1Color: teal500,
+            primary2Color: teal700,
+            primary3Color: grey400,
+            accent1Color: tealA700,
+            accent2Color: grey100,
+            accent3Color: grey500
+        }
+    });
+    
     render() {
+        
         return (
-            <MuiThemeProvider>
+            <MuiThemeProvider muiTheme={this.muiTheme}>
                 <Body />
             </MuiThemeProvider>
         );
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,31 +1,70 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import RaisedButton from "material-ui/RaisedButton";
 import TextField from "material-ui/TextField";
+import Paper from 'material-ui/Paper';
+import Database from '../../pouchdb-starter';
+import PouchDB from "pouchdb";
 import './login.css';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.updateEmail = this.updateEmail.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.verifyLogin = this.verifyLogin.bind(this);
+    }
+    
+    updateEmail(event) {
+        this.setState({email: event.target.value});
+    }
+    
+    updatePassword(event) {
+        this.setState({password: event.target.value});
+    }
+    
+    verifyLogin() {
+        console.log(this.state.email);
+        console.log(this.state.password);
+    }
+    
     render() {
         return (
             <div className="login-wrapper">
-                <div className="login-email-wrapper">
-                    <TextField
-                        floatingLabelText="email"
-                        hintText="your@email.com"
-                        type='email'
-                        style={{width: 300}}
-                    /><br />
-                    <TextField
-                        floatingLabelText="password"
-                        type='password'
-                        hintText="password"
-                        className="input-width"
-                        style={{width: 300}}
-                    /><br />
-                </div>
-                <br />
-                <div className="login-submit">
-                    <RaisedButton label="Log in" primary={true} />
-                </div>
+                <Paper className="login-container" zDepth={2}>
+                    <div className="login-email-wrapper">
+                        <h2>Login</h2>
+                        <br />
+                        <TextField
+                            floatingLabelText="email"
+                            hintText="your@email.com"
+                            type='email'
+                            style={{width: 300}}
+                            value={this.state.email}
+                            onChange={this.updateEmail}
+                        /><br/>
+                        <TextField
+                            floatingLabelText="password"
+                            type='password'
+                            hintText="password"
+                            className="input-width"
+                            style={{width: 300}}
+                            value={this.state.password}
+                            onChange={this.updatePassword}
+                        /><br/>
+                    </div>
+                    <br/>
+                    <div className="login-submit">
+                        <RaisedButton
+                            label="Log in"
+                            primary={true}
+                            onClick={this.verifyLogin}
+                        />
+                    </div>
+                </Paper>
             </div>
         );
     }
